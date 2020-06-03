499a87306d48c92af3633f2748156463609cb394
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2020-06-03 15:08:32 -0600

    End of day catch up #1

--- 21779/vitalstatistics.sql
+++ 21779/vitalstatistics.sql
@@ -1,9 +1,11 @@
 WITH
 
-  -- Deaths 2015 to 2017
-  events00 AS
+  -- All available versions of death data
+  deathversions AS
   (
+    -- Deaths 2015 to 2017
     SELECT
+      2017 recordyear,
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.stkh_num_1) uliabphn,
       ab_hzrd_rts_anlys.hazardutilities.cleansex(a0.sex) sex,
       CASE
@@ -19,12 +21,11 @@ WITH
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.stkh_num_1) IS NOT NULL
       AND
       a0.dethdate BETWEEN COALESCE(a0.birth_date, a0.dethdate) AND TRUNC(SYSDATE, 'MM')
-  ),
+    UNION ALL
   
-  -- Deaths 2010 to 2016
-  events01 AS
-  (
+    -- Deaths 2010 to 2016
     SELECT
+      2016 recordyear,
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.stkh_num_1) uliabphn,
       ab_hzrd_rts_anlys.hazardutilities.cleansex(a0.sex) sex,
       CASE
@@ -40,12 +41,11 @@ WITH
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.stkh_num_1) IS NOT NULL
       AND
       a0.dethdate BETWEEN COALESCE(a0.birth_date, a0.dethdate) AND TRUNC(SYSDATE, 'MM')
-  ),
+    UNION ALL
   
-  -- Deaths 2003 to 2010
-  events02 AS
-  (
+    -- Deaths 2003 to 2010
     SELECT
+      2010 recordyear,
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.stkh_num_1) uliabphn,
       ab_hzrd_rts_anlys.hazardutilities.cleansex(a0.sex) sex,
       CASE
@@ -61,12 +61,11 @@ WITH
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.stkh_num_1) IS NOT NULL
       AND
       a0.dethdate BETWEEN COALESCE(a0.birth_date, a0.dethdate) AND TRUNC(SYSDATE, 'MM')
-  ),
-
-  -- Deaths early
-  events03 AS
-  (
+    UNION ALL
+  
+    -- Deaths early
     SELECT
+      2002 recordyear,
       ab_hzrd_rts_anlys.hazardutilities.cleanphn(a0.primary_uli) uliabphn,
       ab_hzrd_rts_anlys.hazardutilities.cleansex(a0.sex) sex,
       CASE
@@ -82,4 +81,32 @@ WITH
       hazardutilities.cleanphn(a0.primary_uli) IS NOT NULL
       AND
       a0.dethdate BETWEEN COALESCE(a0.birth_date, a0.dethdate) AND TRUNC(SYSDATE, 'MM')
+  ),
+
+  -- Determine the latest release of each death record
+  latestversion AS
+  (
+    SELECT
+      MAX(a0.recordyear) recordyear,
+      a0.uliabphn
+    FROM
+      deathversions a0
+    GROUP BY
+      a0.uliabphn
   )
+
+-- Return the latest record
+SELECT
+  a0.recordyear,
+  a0.uliabphn,
+  a0.sex,
+  a0.birthdate,
+  a0.deceaseddate
+FROM
+  deathversions a0
+  INNER JOIN
+  latestversion a1
+  ON
+    a0.uliabphn = a1.uliabphn
+    AND
+    a0.recordyear = a1.recordyear
