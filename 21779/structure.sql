d4d4d8621881204de282ea75cfac0f5adfd4b46d
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2020-06-08 10:18:57 -0600

    Move columns

--- 21779/structure.sql
+++ 21779/structure.sql
@@ -2,7 +2,7 @@ CREATE TABLE randompseudonym
 (
   uliabphn INTEGER,
   ulipseudonym INTEGER,
+  uploadrequest INTEGER,
   uploaddate DATE GENERATED (SYSDATE),
-  uploaduser VARCHAR2(4000) GENERATED (USER),
-  uploadrequest INTEGER
+  uploaduser VARCHAR2(4000) GENERATED (USER)
 );
