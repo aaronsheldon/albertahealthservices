569765438a376623ecee6ad55a6d6c346d78cd9a
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2020-06-08 09:20:58 -0600

    Correct implementation

---
+++ 21779/implementation.sql
@@ -0,0 +1,8 @@
+
+CREATE OR REPLACE PACKAGE BODY 21779 AS
+  
+  FUNCTION getvitalstatistics RETURN vitalstatistics PIPELINED;
+  
+  FUNCTION getpharmacydispenses RETURN pharmacydispenses PIPELINED;
+
+END 21779
