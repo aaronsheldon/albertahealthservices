1c837cceeeda9da092990b51787ba34c71b2dd4f
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2020-06-08 10:16:53 -0600

    Only need upload date

--- 21779/structure.sql
+++ 21779/structure.sql
@@ -4,6 +4,5 @@ CREATE TABLE randompseudonym
   ulipseudonym INTEGER,
   uploaddate DATE GENERATED (SYSDATE),
   uploaduser VARCHAR2(4000) GENERATED (USER),
-  uploadrequest INTEGER,
-  uploadcount INTEGER
+  uploadrequest INTEGER
 );
