2975f9464311705a564a3142d8424a6a823c60d8
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2020-06-09 09:30:44 -0600

    retrieve latest upload #1

--- 21779/implementation.sql
+++ 21779/implementation.sql
@@ -1,6 +1,19 @@
 
 CREATE OR REPLACE PACKAGE BODY 21779 AS
-  
+
+  CURSOR getlatestupload(queryuser IN VARCHAR2) RETURN DATE IS
+  SELECT
+    MAX(a0.uploaddate) uploadate
+  FROM
+    randompseudonym a0
+  WHERE
+    a0.uploadrequest = 21779
+    AND
+    a0.uploaduser = queryuser
+  GROUP BY
+    a0.uploadrequest,
+    a0.uploaduser;
+
   FUNCTION getvitalstatistics RETURN vitalstatistics PIPELINED;
   
   FUNCTION getpharmacydispenses RETURN pharmacydispenses PIPELINED;
