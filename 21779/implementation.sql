f8c070c16e07d09fbb2e7e97fdb47c1d9902b90c
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2020-06-11 14:52:02 -0600

    Boilerplate pipelined functions #4 and #5

--- 21779/implementation.sql
+++ 21779/implementation.sql
@@ -1,21 +1,29 @@
-
 CREATE OR REPLACE PACKAGE BODY 21779 AS
 
-  CURSOR getlatestupload(queryuser IN VARCHAR2) RETURN DATE IS
-  SELECT
-    MAX(a0.uploaddate) uploadate
-  FROM
-    randompseudonym a0
-  WHERE
-    a0.uploadrequest = 21779
-    AND
-    a0.uploaduser = queryuser
-  GROUP BY
-    a0.uploadrequest,
-    a0.uploaduser;
+  FUNCTION getvitalstatistics RETURN vitalstatistics PIPELINED AS
+    CURSOR localcursor RETURN vitalstatistic;
+    localrecord vitalstatistic;
+    localquery CONSTANT VARCHAR2(4000) := '';
+  BEGIN
+    OPEN localcursor FOR localquery;
+    LOOP
+      FETCH localcursor INTO localrecord
+      EXIT WHEN localcursor%NOTFOUND;
+      PIPE ROW (localrecord);
+    END LOOP;
+  END getvitalstatistics;
 
-  FUNCTION getvitalstatistics RETURN vitalstatistics PIPELINED;
-  
-  FUNCTION getpharmacydispenses RETURN pharmacydispenses PIPELINED;
+  FUNCTION getpharmacydispenses RETURN pharmacydispenses PIPELINED AS
+    CURSOR localcursor RETURN pharmacydispense;
+    localrecord pharmacydispense;
+    localquery CONSTANT VARCHAR2(4000) := '';
+  BEGIN
+    OPEN localcursor FOR localquery;
+    LOOP
+      FETCH localcursor INTO localrecord
+      EXIT WHEN localcursor%NOTFOUND;
+      PIPE ROW (localrecord);
+    END LOOP;
+  END getpharmacydispenses;
 
 END 21779
--- 21779/interface.sql
+++ 21779/interface.sql
@@ -2,30 +2,33 @@ CREATE OR REPLACE PACKAGE 21779 AUTHID DEFINER AS
 
   TYPE vitalstatistic IS RECORD
   (
-    uliabphn INTEGER
+    uliabphn INTEGER,
+    ulipseudonym INTEGER,
+    deceaseddate DATE
   );
-  
-  TYPE vitalstatistics IS TABLE OF vitalstatistic;
-  
+
   TYPE pharmacydispense IS RECORD
   (
-    uliabphn INTEGER
+    uliabphn INTEGER,
+    ulipseudonym INTEGER,
+    dispensedate DATE,
+    registereddin INTEGER,
+    atcclassification VARCHAR2(8),
+    suppliedquantity INTEGER,
+    unitscode VARCHAR2(64),
+    unitsdescription VARCHAR2(2048),
+    siteidentifier VARCHAR2(4),
+    supplieddays INTEGER,
+    standardtherapeutic INTEGER,
+    controlledtherapeutic INTEGER
   );
-  
+
+  TYPE vitalstatistics IS TABLE OF vitalstatistic;
+
   TYPE pharmacydispenses IS TABLE OF pharmacydispense;
 
-  CURSOR getvitalstatistic RETURN vitalstatistic;
-  
-  CURSOR getpharmacydispense RETURN pharmacydispense;
-  
-  CURSOR getlatestupload(queryuser IN VARCHAR2) RETURN DATE;
-  
   FUNCTION getvitalstatistics RETURN vitalstatistics PIPELINED;
   
   FUNCTION getpharmacydispenses RETURN pharmacydispenses PIPELINED;
-  
-  vitalstatisticsquery CONSTANT VARCHAR2(4000) := '';
-  
-  pharmacydispensesquery CONSTANT VARCHAR2(4000) := '';
 
 END 21779
